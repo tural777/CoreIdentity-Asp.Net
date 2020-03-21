3bd339ada9ec8be3eb9b92e81a489345bc58f45e
Tural Novruzov <TuraliNovruzov@outlook.com>
2020-03-22 02:16:49 +0400

    Creating Roles

--- CoreIdentity/Controllers/AccountController.cs
+++ CoreIdentity/Controllers/AccountController.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using CoreIdentity.Models;
 
-namespace Core2Identity.Controllers
+namespace CoreIdentity.Controllers
 {
     [Authorize]
     public class AccountController : Controller
--- CoreIdentity/Infrastructure/CustomUserValidator.cs
+++ CoreIdentity/Infrastructure/CustomUserValidator.cs
@@ -1,8 +1,5 @@
 ﻿using CoreIdentity.Models;
 using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace CoreIdentity.Infrastructure
