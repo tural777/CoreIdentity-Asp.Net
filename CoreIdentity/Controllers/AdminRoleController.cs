c755e29bbaafdcda1bf30015d820a3a82719f795
Tural Novruzov <TuraliNovruzov@outlook.com>
2020-03-22 03:19:30 +0400

    Deleting Roles

--- CoreIdentity/Controllers/AdminRoleController.cs
+++ CoreIdentity/Controllers/AdminRoleController.cs
@@ -16,15 +16,21 @@ namespace CoreIdentity.Controllers
             roleManager = _roleManager;
         }
 
+
+
         public IActionResult Index()
         {
             return View(roleManager.Roles);
         }
 
+
+
         public IActionResult Create()
         {
             return View();
         }
+
+
 
         [HttpPost]
         public async Task<IActionResult> Create(string name)
@@ -46,5 +52,35 @@ namespace CoreIdentity.Controllers
             }
             return View(name);
         }
+
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+
+            if (role != null)
+            {
+                var result = await roleManager.DeleteAsync(role);
+
+                if (result.Succeeded)
+                {
+                    TempData["message"] = $"{role.Name} has been deleted.";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
