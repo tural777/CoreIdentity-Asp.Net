22c2c52a43ac77dadcdf051f509ad42ce7e21db4
Tural Novruzov <TuraliNovruzov@outlook.com>
2020-03-22 17:56:51 +0400

    Managing User Roles

--- CoreIdentity/Controllers/AdminRoleController.cs
+++ CoreIdentity/Controllers/AdminRoleController.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
+using CoreIdentity.Models;
 
 namespace CoreIdentity.Controllers
 {
     public class AdminRoleController : Controller
     {
         private RoleManager<IdentityRole> roleManager;
+        private UserManager<ApplicationUser> userManager;
 
-        public AdminRoleController(RoleManager<IdentityRole> _roleManager)
+        public AdminRoleController(RoleManager<IdentityRole> _roleManager, UserManager<ApplicationUser> _userManager)
         {
             roleManager = _roleManager;
+            userManager = _userManager;
         }
 
 
@@ -53,6 +56,88 @@ namespace CoreIdentity.Controllers
             return View(name);
         }
 
+
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+
+            var members = new List<ApplicationUser>();
+            var nonmembers = new List<ApplicationUser>();
+
+            foreach (var user in userManager.Users)
+            {
+                var list = await userManager.IsInRoleAsync(user, role.Name)
+                    ? members : nonmembers;
+
+                list.Add(user);
+            }
+
+            var model = new RoleDetails()
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonmembers
+            };
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(RoleEditModel model)
+        {
+            IdentityResult result;
+
+            if (ModelState.IsValid)
+            {
+                foreach (var userId in model.IdsToAdd ?? new string[] { })
+                {
+                    var user = await userManager.FindByIdAsync(userId);
+
+                    if (user != null)
+                    {
+                        result = await userManager.AddToRoleAsync(user, model.RoleName);
+
+                        if (!result.Succeeded)
+                        {
+                            foreach (var error in result.Errors)
+                            {
+                                ModelState.AddModelError("", error.Description);
+                            }
+                        }
+                    }
+                }
+
+                foreach (var userId in model.IdsToDelete ?? new string[] { })
+                {
+                    var user = await userManager.FindByIdAsync(userId);
+
+                    if (user != null)
+                    {
+                        result = await userManager.RemoveFromRoleAsync(user, model.RoleName);
+
+                        if (!result.Succeeded)
+                        {
+                            foreach (var error in result.Errors)
+                            {
+                                ModelState.AddModelError("", error.Description);
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Edit", model.RoleId);
+            }
+        }
 
 
 
---
+++ CoreIdentity/Models/RoleModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace CoreIdentity.Models
+{
+    public class RoleDetails
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<ApplicationUser> Members { get; set; }
+        public IEnumerable<ApplicationUser> NonMembers { get; set; }
+    }
+
+    public class RoleEditModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+
+        public string[] IdsToAdd{ get; set; }
+        public string[] IdsToDelete { get; set; }
+    }
+}
